4a0e5a4d68a1f8b62fd8faa605efb98c5192f856
rpgdeveloper707 <rpg_developer707@mail.ru>
2022-08-13 00:42:00 +0300

    fixed emote name

--- src/main/java/com/example/screamlarkbot/utils/Emotes.java
+++ src/main/java/com/example/screamlarkbot/utils/Emotes.java
@@ -9,7 +9,7 @@ public enum Emotes {
     FROG_WAVE("FrogWave"),
     LIZARD_PLS("lizardPls"),
     FEELS_WEAK_MAN("FeelsWeakMan"),
-    PEEPO_CLAP("peepoCLap");
+    PEEPO_CLAP("peepoClap");
 
     private final String name;
 }
