c6631b13134dea53b11d07b50188e1018985ca67
rpgdeveloper707 <rpg_developer707@mail.ru>
2022-08-12 20:46:08 +0300

    fixed listeners

--- src/main/java/com/example/screamlarkbot/configs/BotConfiguration.java
+++ src/main/java/com/example/screamlarkbot/configs/BotConfiguration.java
@@ -4,10 +4,16 @@ import com.example.screamlarkbot.utils.Emotes;
 import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.helix.domain.User;
+import com.github.twitch4j.helix.domain.UserList;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
+@Slf4j
 @Configuration
 public class BotConfiguration {
 
@@ -28,8 +34,32 @@ public class BotConfiguration {
                 .build();
         client.getChat().joinChannel(channelName);
         client.getChat().sendMessage(channelName, Emotes.FROG_WAVE.getName());
-        client.getClientHelper().enableFollowEventListener(channelName);
-        client.getClientHelper().enableStreamEventListener(channelName);
+        enableFollowEventListener(client);
+        enableStreamEventListener(client);
         return client;
+    }
+
+    private void enableFollowEventListener(TwitchClient client) {
+        String token = oauth.substring("oauth:".length());
+        UserList users = client.getHelix().getUsers(token, null, Collections.singletonList(channelName)).execute();
+
+        if (users.getUsers().size() == 1) {
+            User user = users.getUsers().get(0);
+            client.getClientHelper().enableFollowEventListener(user.getId(), user.getLogin());
+        } else {
+            log.error("Failed to add channel " + channelName + " to Follow Listener, maybe it doesn't exist!");
+        }
+    }
+
+    private void enableStreamEventListener(TwitchClient client) {
+        String token = oauth.substring("oauth:".length());
+        UserList users = client.getHelix().getUsers(token, null, Collections.singletonList(channelName)).execute();
+
+        if (users.getUsers().size() == 1) {
+            User user = users.getUsers().get(0);
+            client.getClientHelper().enableStreamEventListener(user.getId(), user.getLogin());
+        } else {
+            log.error("Failed to add channel {} to stream event listener!", channelName);
+        }
     }
 }
