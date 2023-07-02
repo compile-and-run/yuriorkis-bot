35a018a10fd7bad7fe37c001edbc605966b2b075
compile-and-run <87780204+compile-and-run@users.noreply.github.com>
2023-07-02 13:08:56 +0200

    Add files via upload

---
+++ src/main/java/com/example/screamlarkbot/services/Chat.java
@@ -0,0 +1,29 @@
+package com.example.screamlarkbot.services;
+
+import com.example.screamlarkbot.models.gpt.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chat {
+    private final int MAX_SIZE = 20;
+    private final List<Message> messages = new ArrayList<>();
+
+    public synchronized void addUserMessage(String name, String message) {
+        messages.add(new Message("user", name, message));
+        if (messages.size() > MAX_SIZE) {
+            messages.remove(0);
+        }
+    }
+
+    public synchronized void addBotMessage(String message) {
+        messages.add(new Message("assistant", null, message));
+        if (messages.size() > MAX_SIZE) {
+            messages.remove(0);
+        }
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+}
